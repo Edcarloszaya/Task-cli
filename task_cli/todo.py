17138f37f4012ec5d4a760ad44abb9de14d81645
Edcarlos <Edcarlos.molequedoido@gmail.com>
2024-09-19 18:16:41 -0300

    Adicao dos metados add e create_id:
    add : recebe a task e como argumento e salva no arquivo.
    
    create_id: cria um id unico pra cada task!

--- task_cli/todo.py
+++ task_cli/todo.py
@@ -1,4 +1,4 @@
-import time 
+from datetime import datetime
 import json 
 import os
 
@@ -9,6 +9,32 @@ class TodoList:
 
     
     def save_tasks(self):
+        ''' salva a task'''
+
         with open(self.filepath,"w") as file:
             json.dump(self.tasks,file,indent=4)
 
+    def add(self,description):
+        ''' cria uma task'''
+
+        task = {
+        "id":self.create_id(),
+        "description":f'{description}',
+        "status":"in-progress",
+        "createdAt":datetime.now().isoformat(),
+        "updateAt":"",
+        }
+        self.tasks.append(task)
+        self.save_tasks()
+
+    def create_id(self):
+        ''' cria um id unico'''
+
+        if not self.tasks:
+            return 1
+        else:
+            max_1 = max(task["id"] for task in self.tasks)
+            return max_1 + 1
+        
+# td = TodoList()
+# td.add("test")
