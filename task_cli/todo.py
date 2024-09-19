59d9acffaa94f90d7a806d6a4e2a316ddd01490f
Edcarlos <Edcarlos.molequedoido@gmail.com>
2024-09-19 18:03:32 -0300

    adicao save_tasks : salva as tasks no arquivo Json

--- task_cli/todo.py
+++ task_cli/todo.py
@@ -3,7 +3,12 @@ import json
 import os
 
 class TodoList:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,filepath='tasks.json') -> None:
+        self.filepath = filepath
+        self.tasks = []
 
     
+    def save_tasks(self):
+        with open(self.filepath,"w") as file:
+            json.dump(self.tasks,file,indent=4)
+
