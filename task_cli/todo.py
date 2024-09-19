d9b2f847e5d792ef0b5704f954d014e342e92416
Edcarlos <Edcarlos.molequedoido@gmail.com>
2024-09-19 18:39:21 -0300

    Adicao metados update e load_task:
    load_task : retorna uma lista de task, se nao tiver retorna uma lista vazia.
    
    update: Atualiza a task com base no Id fornecido.

--- task_cli/todo.py
+++ task_cli/todo.py
@@ -5,7 +5,7 @@ import os
 class TodoList:
     def __init__(self,filepath='tasks.json') -> None:
         self.filepath = filepath
-        self.tasks = []
+        self.tasks = self.load_tasks()
 
     
     def save_tasks(self):
@@ -35,6 +35,32 @@ class TodoList:
         else:
             max_1 = max(task["id"] for task in self.tasks)
             return max_1 + 1
-        
-# td = TodoList()
-# td.add("test")
+
+    def update(self,id,new_description):
+        ''' edita a task com base no id'''
+
+        for task in self.tasks:
+            if task["id"] == id:
+                task["description"] = new_description
+                task["updateAt"] = datetime.now().isoformat()
+                self.save_tasks()
+                print(f'task updated')
+                return
+            
+            else:
+                print(f"Task with ID {id} not found.")
+          
+    def load_tasks(self):
+        """Carrega as tarefas de um arquivo JSON ou cria o arquivo se não existir."""
+        if not os.path.exists(self.filepath):
+            # Se o arquivo não existir, cria um arquivo JSON vazio
+            with open(self.filepath, 'w') as file:
+                json.dump([], file)
+            return []
+        else:
+            with open(self.filepath, 'r') as file:
+                return json.load(file)
+
+
+td = TodoList()
+td.update(1,"testando")
