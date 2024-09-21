f784590de6ec9662ed5056923e089f7bd4f41de6
Edcarlos <Edcarlos.molequedoido@gmail.com>
2024-09-20 22:04:59 -0300

    Instaciada a classe da TodoList , e organizado a gerenciamento do fluxo do task-cli pelo terminal pra entrada de comando do usuario,renomiado comando

--- task_cli/task-cli.py
+++ task_cli/task-cli.py
@@ -1,4 +1,5 @@
 import argparse
+from todo import TodoList
 
 parser = argparse.ArgumentParser(description="To-Do List CLI App")
 
@@ -19,7 +20,7 @@ delete = subparsers.add_parser("delete", help="delete a task from id")
 delete.add_argument("id", type=int, help="id of the task that will be deleted")
 
 # Comando para completar uma tarefa
-mark_done = subparsers.add_parser("mark-in-done", help="mark done task from id")
+mark_done = subparsers.add_parser("mark-done", help="mark done task from id")
 mark_done.add_argument("id", type=int, help="id of the task that will be marked done")
 
 # Comando para marca que pendente a task
@@ -48,3 +49,33 @@ list_in_progress = subparsers.add_parser(
 # Parse dos argumentos
 args = parser.parse_args()
 
+# estanciando  a classe 
+todo = TodoList()
+
+# logica pra operacaoes na todo list
+if args.command == "add":
+    todo.add(args.description)
+
+elif args.command == "update":
+    todo.update(args.id,args.description)
+
+elif args.command == "delete":
+    todo.delete(args.id)
+
+elif args.command == "mark-done":
+    todo.mark_done(args.id)
+    
+elif args.command == "mark-in-progress":
+    todo.mark_in_progress(args.id)
+
+elif args.command == "list":
+    todo.list()
+
+elif args.command == "list_done":
+    todo.list_done("done")
+
+elif args.command == "list_in-progress":
+    todo.list_in_progress("in-progress")
+    
+else:
+    parser.print_help()
