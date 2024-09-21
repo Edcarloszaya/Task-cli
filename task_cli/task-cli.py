761114df73642950677eefa53d58da8066cbcfa5
Edcarlos <Edcarlos.molequedoido@gmail.com>
2024-09-20 21:16:03 -0300

    renomeada nome e arguemento dos comando que tava em conflito ! escrito errado

--- task_cli/task-cli.py
+++ task_cli/task-cli.py
@@ -10,23 +10,23 @@ add = subparsers.add_parser("add", help="add task to the Todo-list")
 add.add_argument("description", type=str, help="descrition from the task ")
 
 # Comando para atualiza as tarefas
-update = subparsers.add_parser("add", help="update task to the Todo-list")
-update.add_subparsers("id", type=str, help="id of the task that will be updated ")
-parser.add_argument("description", type=str, help="descrition from the task ")
+update = subparsers.add_parser("update", help="update task to the Todo-list")
+update.add_argument("id", type=int, help="id of the task that will be updated ")
+update.add_argument("description", type=str, help="descrition from the task ")
 
 # Comando para deleta uma tarefa
 delete = subparsers.add_parser("delete", help="delete a task from id")
 delete.add_argument("id", type=int, help="id of the task that will be deleted")
 
 # Comando para completar uma tarefa
-mark_done = subparsers.add_parser("mark-in-progress", help="mark done task from id")
-delete.add_argument("id", type=int, help="id of the task that will be marked done")
+mark_done = subparsers.add_parser("mark-in-done", help="mark done task from id")
+mark_done.add_argument("id", type=int, help="id of the task that will be marked done")
 
 # Comando para marca que pendente a task
 marke_in_progress = subparsers.add_parser(
     "mark-in-progress", help="mark in-progress task from id"
 )
-delete.add_argument(
+marke_in_progress.add_argument(
     "id", type=int, help="id of the task that will be marked in-progress"
 )
 
@@ -34,13 +34,17 @@ delete.add_argument(
 list_all = subparsers.add_parser("list", help="list all stored tasks")
 
 # Comando para listar todas as tarefas com satus completo
-list_done = subparsers.add_parser("list", help="list all stored tasks marked done")
-list_done.add_argument("status", type=str, help="status from the task")
+list_done = subparsers.add_parser(
+    "list_done", help="list all stored tasks marked done"
+)
+
 
 # Comando para listar todas as tarefas com satus pendente
 list_in_progress = subparsers.add_parser(
-    "list", help="list all stored tasks marked in-progress"
+    "list_in-progress", help="list all stored tasks marked in-progress"
 )
-list_in_progress.add_argument("status", type=str, help="status from the task")
 
+
+# Parse dos argumentos
 args = parser.parse_args()
+
